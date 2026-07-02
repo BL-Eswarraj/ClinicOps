71b125c21e7b6787e461e08af4a993a09a2e1432
BL-Eswarraj <eswarraj.rajendiren@bridgelabz.com>
2026-07-02 15:45:34 +0530

    Eswar Added UC7: Added functionality for patient registration

---
+++ ClinicOpsApp/src/Patient.java
@@ -0,0 +1,35 @@
+public class Patient {
+
+    // Instance Variables
+    private String id;
+    private String name;
+    private String gender;
+    private int age;
+    private String mobileNumber;
+
+    // Constructor
+    public Patient(String id,
+                   String name,
+                   String gender,
+                   int age,
+                   String mobileNumber) {
+
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.mobileNumber = mobileNumber;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Patient ID    : " + id +
+               "\nName           : " + name +
+               "\nGender         : " + gender +
+               "\nAge            : " + age +
+               "\nMobile Number  : " + mobileNumber +
+               "\n------------------------------------";
+    }
+
+}
